052daca255071e0b6b5767bdb5c6b990fa019ed4
jairoarivas <jairoalbertorivas@gmail.com>
2018-02-24 16:47:19 -0500

    Created two scenes for the demo, added script called changeScenes to change between scenes
    Organized project directory a little by placing all scenes in a scene folder. Created a prefab folder for the demo. Imported custom made house for use in the demo. Created a scripts folder for scripts. Added a changeScenes script with a template code that can be used to change between scenes. Created two scenes, one with a piece of cheese on the table, and one with an apple on the table. Placed camera at table.

---
+++ PainVR/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//needed to change scenes
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //function to change scenes
+    //replace SceneName with name of scene you want to change to
+    /*
+    void changeScene()
+    {
+        SceneManager.LoadScene("SceneName");
+    }
+    */
+}

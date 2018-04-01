e8b504655e20580c23d4343deb18819f1d286b31
Joe Knight <knightjoe2222@gmail.com>
2018-04-01 15:27:22 -0400

    added main menu to project reboot
    -added folders for:
     -fonts
     -scripts
     -scenes

---
+++ PainVR/Assets/Scripts/SliderText.cs
@@ -0,0 +1,14 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class SliderText : MonoBehaviour 
+ {
+     string sliderTextString = "0";
+     public Text sliderText; 
+ 
+     public void textUpdate (float textUpdateNumber)
+     {
+         sliderTextString = textUpdateNumber.ToString();
+         sliderText.text = sliderTextString;
+     }
+ }

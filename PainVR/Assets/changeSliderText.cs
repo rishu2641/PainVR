4902445f82824943d99c203c0dcf3a75ae806a26
Joe Knight <knightjoe2222@gmail.com>
2018-04-02 15:20:35 -0400

    added slider value text above slider

---
+++ PainVR/Assets/changeSliderText.cs
@@ -0,0 +1,14 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class changeSliderText : MonoBehaviour {
+ 
+     public Text sliderValue;
+	 public Slider slider;
+	 
+	 void Update(){
+	 
+	 sliderValue.text = slider.value.ToString("0.0");
+	 
+	 }
+ }

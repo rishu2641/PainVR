4cf4912824a75db18627eb4671256095fbbdcc77
jairoarivas <jairoalbertorivas@gmail.com>
2018-04-02 22:03:17 -0400

    Finished Touching Up on Slider on Character Controller

--- PainVR/Assets/ToggleSlider.cs
+++ PainVR/Assets/ToggleSlider.cs
@@ -2,27 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 
 public class ToggleSlider : MonoBehaviour {
 
 	public GameObject canvas;
-	public Text sliderText; 
+	public Text sliderText;
+	public Slider slider;
+	double startingtime;
+	double heldTimeA;
+	double heldTimeB;
+	bool isActive;
+	public int maxValue = 100;
+	public int minValue = 0;
+	int val;
+
+	public static double ConvertToUnixTimestamp(DateTime date){
+	    DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+	    TimeSpan diff = date.ToUniversalTime() - origin;
+	    return Math.Floor(diff.TotalSeconds);
+	}
 
 	public void toggleAndIncrement(){
+		startingtime = ConvertToUnixTimestamp(DateTime.Now);
 		canvas.SetActive(true);
-		sliderText.text = int.Parse(sliderText.text + 1).ToString();
-
+		if(int.Parse(sliderText.text) < maxValue){
+			val = int.Parse(sliderText.text) + 1;
+		}
+		else{
+			val = maxValue;
+		}
+		sliderText.text = val.ToString();
+		slider.value = val;
+		isActive = true;
+	}
+	public void toggleAndDecrement(){
+		startingtime = ConvertToUnixTimestamp(DateTime.Now);
+		canvas.SetActive(true);
+		if(int.Parse(sliderText.text) > minValue){
+			val = int.Parse(sliderText.text) - 1;
+		}
+		else{
+			val = minValue;
+		}
+		sliderText.text = val.ToString();
+		slider.value = val;
+		isActive = true;
 	}
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(OVRInput.Get(OVRInput.Button.One)){
+		if(OVRInput.GetDown(OVRInput.Button.One)){
+			heldTimeA = ConvertToUnixTimestamp(DateTime.Now);
 			toggleAndIncrement();
 		}
+		else if(OVRInput.Get(OVRInput.Button.One)){
+			if(ConvertToUnixTimestamp(DateTime.Now) - heldTimeA >= .75){
+				toggleAndIncrement();
+			}
+		}
+		if(OVRInput.GetDown(OVRInput.Button.Two)){
+			heldTimeB = ConvertToUnixTimestamp(DateTime.Now);
+			toggleAndDecrement();
+		}
+		else if(OVRInput.Get(OVRInput.Button.Two)){
+			if(ConvertToUnixTimestamp(DateTime.Now) - heldTimeB >= .75){
+				toggleAndDecrement();
+			}
+		}
+		if(isActive && ConvertToUnixTimestamp(DateTime.Now) - startingtime >= 5){
+        	canvas.SetActive(false);
+        	isActive = false;
+		} 
 	}
+
 }

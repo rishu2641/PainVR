db1df895b681b4e2a2fdca1ea1d2c16812639aa6
Joe Knight <knightjoe2222@gmail.com>
2018-04-02 20:07:28 -0400

    added patient file initialization functionality

---
+++ PainVR/Assets/WriteToFile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+
+public class WriteToFile : MonoBehaviour {
+
+	public void WriteFile(){
+		 System.IO.File.WriteAllText("C:\\Users\\Joe\\Desktop\\yourtextfile.csv", "Does this work?");
+	}
+}

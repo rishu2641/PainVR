14eb14abff9523daee335f8f3cf335465da4f126
Joe Knight <knightjoe2222@gmail.com>
2018-04-02 23:34:32 -0400

    made the file output directory C:/

--- PainVR/Assets/Scripts/InitializePatientFile.cs
+++ PainVR/Assets/Scripts/InitializePatientFile.cs
@@ -15,7 +15,7 @@ public class InitializePatientFile : MonoBehaviour {
 		patientName = iField.text;
 		if(patientName.Trim() != ""){
 			patientName = patientName.Trim().Replace(" ", "_");
-	 		System.IO.File.WriteAllText("C:\\Users\\Joe\\Desktop\\Patient_" + patientName + "_Opal_Testing.csv", "Patient's Name:,"+patientName+",\n" + ",,\n" + "Scene,Time,Anxiety Level\n");
+	 		System.IO.File.WriteAllText("C:\\Patient_" + patientName + "_Opal_Testing.csv", "Patient's Name:,"+patientName+",\n" + ",,\n" + "Scene,Time,Anxiety Level\n");
 			SceneManager.LoadScene("apple-1-supermarket");
 		}
 	}

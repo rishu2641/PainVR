cc4243ab35993cfd668083639867f77ae61e5e02
jairoarivas <jairoalbertorivas@gmail.com>
2018-04-30 23:57:12 -0400

    sounds and people animation for apple heirarchy

---
+++ PainVR/Assets/PlayerCurnchingApple.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCurnchingApple : MonoBehaviour {
+	public GameObject head;
+	public bool yes = false;
+	// Update is called once per frame
+	void Update () {
+		
+		float posX = head.transform.position.x;
+		float posY = head.transform.position.y-.1f;
+		float posZ = head.transform.position.z;
+
+		float distanceX = posX-transform.position.x;
+		float distanceY = posY-transform.position.y;
+		float distanceZ = posZ-transform.position.z;
+
+		float distance = Mathf.Sqrt(distanceX*distanceX - distanceY*distanceY - distanceZ*distanceZ);
+
+		if(distance < .2){
+			yes = true;
+			AudioSource audio = GetComponent<AudioSource>();
+        	audio.Play();
+		}
+		else{yes =false;}
+        
+
+        
+           
+    
+	}
+}
